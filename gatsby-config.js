823666dc6afe7416b09a0a21f997bd7aad940423
GitDarren <darren321@gmail.com>
2018-05-03 16:46:40 -0500

    adding background image via GraphQL query

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,19 @@
 module.exports = {
   siteMetadata: {
     title: 'Darren Smith Portfolio',
-    desc: "A new website and blog for Darren Smith, full stack developer",
+    desc: 'A new website and blog for Darren Smith, full stack developer',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'img',
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
 }
